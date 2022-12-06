689992815c5633bab9a196b7ddf4402ce3108fe0
Nur-M-Arief-K <ariefnur141@gmail.com>
2022-12-06 12:32:50 +0700

    test fetching data from unsplash

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
-console.log("testing");
+// import { API_KEY } from "./unsplash-key";
+
+// unsplash API
+const count = 10;
+const apiKey = "AETn99UXiyxB-y8JeJ6spFcl--yFebF_b0vMuxHy5Ts";
+const apiURL = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
+
+// get photos from unsplash API
+async function getPhotos() {
+    try {
+        const response = await fetch(apiURL);
+        const data = await response.json();
+        console.log(data);
+    } catch (error) {
+        
+    }
+}
+
+//onload
+getPhotos();
+
+// apikey AETn99UXiyxB-y8JeJ6spFcl--yFebF_b0vMuxHy5Ts
+// secret key lqhdujMa8kPWCkX7TYHL9fskoITDWyTYg_OGP4e_mX0
